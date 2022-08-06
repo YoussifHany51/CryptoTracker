df749057dac9458a47c69f12562c1ab60dbc4958
Youssif Hany <youssifhany962@gmail.com>
2022-08-06 21:20:25 +0200

    Initial Commit

---
+++ DailyCrypto/DailyCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyCryptoApp.swift
+//  DailyCrypto
+//
+//  Created by Youssif Hany on 06/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct DailyCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
