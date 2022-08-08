fe508a8e7bc4e0f8bf89c9d99ccb3c530d4932ec
Youssif Hany <youssifhany962@gmail.com>
2022-08-08 15:22:20 +0200

    Create Model & Home View

--- DailyCrypto/Core/Home/Views/HomeView.swift
+++ DailyCrypto/Core/Home/Views/HomeView.swift
@@ -8,13 +8,59 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @State private var showPortfolio: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            VStack{
+                HomeHeader
+                Spacer(minLength: 0)
+            }
+        }
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
+        NavigationView{
         HomeView()
+                .navigationBarHidden(true)
+        }
     }
+}
+
+
+extension HomeView{
+    
+    private var HomeHeader : some View{
+        HStack{
+            CircleButtonView(
+                iconName: showPortfolio ? "plus" : "info" )
+            .animation(.none)
+            .background(
+            CircleButtonViewAnimation(animate: $showPortfolio)
+            )
+            Spacer()
+            Text(showPortfolio ? "Portfolio" : "Live Prices")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.theme.accent)
+                .animation(.none)
+            Spacer()
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(
+                    Angle(degrees: showPortfolio ? 180 : 0))
+                .onTapGesture {
+                    withAnimation(.spring()){
+                        showPortfolio.toggle()
+                    }
+                }
+        }
+        .padding(.horizontal)
+    }
+    
 }
