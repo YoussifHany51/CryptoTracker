ad70538f96f5af628cbcbfa4c4c267096ce8c26e
Youssif Hany <youssifhany962@gmail.com>
2022-08-20 20:20:58 +0200

    Market View and Model

--- DailyCrypto/Services/MarketDataService.swift
+++ DailyCrypto/Services/MarketDataService.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import Combine
+
+class MarketDataService {
+    
+    
+    @Published var marketData:MarketDataModel? = nil
+    var marketSubscription: AnyCancellable?
+    
+    init(){
+        getData()
+    }
+    
+    private func getData(){
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/global") else {return}
+        
+        marketSubscription = NetworkManager.downloadURL(url: url)
+            .decode(type: GlobalData.self, decoder: JSONDecoder())
+            .sink(receiveCompletion: NetworkManager.handleCompletion, receiveValue: { [weak self] (returnedGlobalData) in
+                self?.marketData = returnedGlobalData.data
+                self?.marketSubscription?.cancel()
+            })
+    }
+}
+
