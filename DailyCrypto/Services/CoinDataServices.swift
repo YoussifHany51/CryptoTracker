4cd9dc099eda5580aa08d6399d8c2927eedf26b8
Youssif Hany <youssifhany962@gmail.com>
2022-08-24 06:24:01 +0200

    Edit Coin Services

--- DailyCrypto/Services/CoinDataServices.swift
+++ DailyCrypto/Services/CoinDataServices.swift
@@ -37,7 +37,7 @@ class CoinDataServices {
            // 7. store (cancel subscription if needed)
            */
     
-    private func getAllCoins(){
+        func getAllCoins(){
         guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h") else {return}
         
         coinsSubscription = NetworkManager.downloadURL(url: url)
